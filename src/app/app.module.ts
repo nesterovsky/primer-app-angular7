6a419263d16f4334540d777abd608345fd133e27
Gaby Nesterovsky <nesterovsky.ucla@gmail.com>
2019-05-21 22:46:05 -0400

    new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import {environment} from 'src/environments/environment';
 
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {ReactiveFormsModule} from '@angular/forms';
-import { TodoListComponent } from './todo/todo-list/todo-list.component'
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
+import { TodoFormComponent } from './todo/todo-form/todo-form.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    TodoListComponent
+    TodoListComponent,
+    TodoFormComponent
   ],
   imports: [
     BrowserModule,
@@ -26,6 +28,7 @@ import { TodoListComponent } from './todo/todo-list/todo-list.component'
     ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [TodoFormComponent]
 })
 export class AppModule { }
--- src/app/todo/todo-list/todo-list.component.ts
+++ src/app/todo/todo-list/todo-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Alert } from 'selenium-webdriver';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { TodoFormComponent } from '../todo-form/todo-form.component';
 
 @Component({
   selector: 'app-todo-list',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
   }
-
+  ClickAddTodo(){
+    const modal = this.modalService.open(TodoFormComponent);
+    modal.result.then(
+      this.handleModalTodoFormClose.bind(this),
+      this.handleModalTodoFormClose.bind(this)
+    )
+  }
+  handleModalTodoFormClose(){
+    alert('se ha cerrado el modal');
+  }
 }
