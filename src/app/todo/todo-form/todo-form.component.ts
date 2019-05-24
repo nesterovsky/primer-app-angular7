2a8c25629e576404e060ec80e5dd6597d0dece4f
Gaby Nesterovsky <nesterovsky.ucla@gmail.com>
2019-05-23 22:18:18 -0400

    changes todo-form.component.ts

--- src/app/todo/todo-form/todo-form.component.ts
+++ src/app/todo/todo-form/todo-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-form',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoFormComponent implements OnInit {
 
-  constructor() { }
+  todoForm: FormGroup;
+  
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit() {
+    this.todoForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      done: false
+    })
   }
 
 }
