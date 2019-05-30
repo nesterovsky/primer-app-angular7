b41db5f19ca30cd6dbc237e6252e2b170e0a6449
Gaby Nesterovsky <nesterovsky.ucla@gmail.com>
2019-05-29 23:32:18 -0400

    changes enviroment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+    firebase: {
+    apiKey: "AIzaSyCw2DomW9MIZuB5Ka5Z6VQWiWJV1pnlZnw",
+    authDomain: "app-todo-13369.firebaseapp.com",
+    databaseURL: "https://app-todo-13369.firebaseio.com",
+    projectId: "app-todo-13369",
+    storageBucket: "app-todo-13369.appspot.com",
+    messagingSenderId: "262704771830",
+    appId: "1:262704771830:web:e229c347c5c9f600"
+  }
 };
