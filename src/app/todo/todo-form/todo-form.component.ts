40f620bb1831470956f75cc79d21960f6b33b49c
Gaby Nesterovsky <nesterovsky.ucla@gmail.com>
2019-05-29 21:30:20 -0400

    ultimos cambios

--- src/app/todo/todo-form/todo-form.component.ts
+++ src/app/todo/todo-form/todo-form.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { Todo } from '../models/todo';
 import { TodoService } from '../services/todo.service';
+import { TodoViewModel } from '../models/todo-view-model';
 
 
 @Component({
@@ -14,6 +15,8 @@ import { TodoService } from '../services/todo.service';
 export class TodoFormComponent implements OnInit {
 
   todoForm: FormGroup;
+  createMode: boolean = true;
+  todo: TodoViewModel;
   constructor(private formBuilder: FormBuilder,
     public activeModal: NgbActiveModal,
     private todoService: TodoService) { }
@@ -24,22 +27,44 @@ export class TodoFormComponent implements OnInit {
       description: ['', Validators.required],
       done: false
     })
+  
+    if (!this.createMode) {
+      this.loadTodo(this.todo); 
+    }  
+  }
+
+  loadTodo(todo){
+   this.todoForm.patchValue(todo)
   }
 
   saveTodo() {
     if (this.todoForm.invalid) {
       return;
     }
-    let todo: Todo = this.todoForm.value;
-    todo.lastModifiedDate = new Date();
-    todo.createdDate = new Date();
-    this.todoService.saveTodo(todo)
-    .then(response => this.handleSuccessfulSaveTodo(response, todo))
-    .catch(err => console.error(err));
 
+    if (this.createMode){
+      let todo: Todo = this.todoForm.value;
+      todo.lastModifiedDate = new Date();
+      todo.createdDate = new Date();
+      this.todoService.saveTodo(todo)
+      .then(response => this.handleSuccessfulSaveTodo(response, todo))
+      .catch(err => console.error(err));
+    } else{
+      let todo: TodoViewModel = this.todoForm.value;
+      todo.id = this.todo.id;
+      todo.lastModifiedDate = new Date();
+      this.todoService.editTodo(todo)
+        .then(() => this.handleSuccessfulEditTodo(todo))
+        .catch(err => console.error(err));
+    }
   }
+   
   handleSuccessfulSaveTodo(response: DocumentReference, todo: Todo) {
     //enviar la informacion al todo list
     this.activeModal.dismiss({ todo: todo, id: response.id });
+  }
+
+  handleSuccessfulEditTodo(todo: TodoViewModel) {
+    this.activeModal.dismiss({ todo: todo, id: todo.id, createMode: false });
   }
 }
